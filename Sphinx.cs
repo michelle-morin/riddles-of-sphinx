045dadfee10b61bf5aa880ece6b0320a05b99b95
jeremy kale padot, Katy Wicz and Michelle Morin <jeremy@>
2020-02-24 10:06:12 -0800

    add randomization to riddle questioning

--- Sphinx.cs
+++ Sphinx.cs
@@ -1,26 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class  RiddlesOfSphinx
 {
   static void Main()
   {
-    Console.WriteLine("What is big, red, and eats rocks?");
-    string userAnswer = Console.ReadLine();
-    if (userAnswer == "a big red rockeater")
+    AskRiddle();
+  }
+  static void AskRiddle()
+  {
+    string[] riddles = { "What is big, red, and eats rocks?", "What walks on four legs in the morning, 2 legs at noon and 3 legs in the evening?", "What is black and white and red all over?", "what do you call a fish missing an eye?" };
+    string[] riddleAnswers = { "a big red rockeater", "Human", "newspaper", "fsh" };
+    var random = new Random();
+    var randomNumber = random.Next(0, 3);
+    Console.WriteLine(riddles[randomNumber]);
+    string userAnswer  = Console.ReadLine();
+    if (userAnswer == riddleAnswers[randomNumber])
     {
       Console.WriteLine("you defeated the Sphinx!");
-      Console.WriteLine("What walks on four legs in the morning, 2 legs at noon and 3 legs in the evening?");
-      userAnswer = Console.ReadLine();
-      if (userAnswer == "Human")
-      {
-        Console.WriteLine("you defeated the Sphinx!");
-      }
-      else
-      {
-        Console.WriteLine("you have been eaten by the Sphinx!");
-      }
+      AskRiddle();
     }
-    else
+    else 
     {
       Console.WriteLine("you have been eaten by the Sphinx!");
     }
