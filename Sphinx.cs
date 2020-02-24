29822f789e86c78655d5a03950690ed494a281ae
Katy Wicz, Michelle Morin and jeremy kale padot <katy+michelle+jeremy@>
2020-02-24 10:39:24 -0800

    refactor logic to continuously ask random questions until all questions are answered correctly

--- Sphinx.cs
+++ Sphinx.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 
 class  RiddlesOfSphinx
 {
+  public static List<string> riddles = new List<string> { "What is big, red, and eats rocks?", "What walks on four legs in the morning, 2 legs at noon and 3 legs in the evening?", "What is black and white and red all over?", "what do you call a fish missing an eye?" };
+  public static List<string> riddleAnswers = new List<string> { "a big red rockeater", "Human", "newspaper", "fsh" };
   static void Main()
   {
     AskRiddle();
   }
   static void AskRiddle()
   {
-    string[] riddles = { "What is big, red, and eats rocks?", "What walks on four legs in the morning, 2 legs at noon and 3 legs in the evening?", "What is black and white and red all over?", "what do you call a fish missing an eye?" };
-    string[] riddleAnswers = { "a big red rockeater", "Human", "newspaper", "fsh" };
     var random = new Random();
-    var randomNumber = random.Next(0, 3);
+    var randomNumber = random.Next(0, riddles.Count);
     Console.WriteLine(riddles[randomNumber]);
     string userAnswer  = Console.ReadLine();
     if (userAnswer == riddleAnswers[randomNumber])
     {
-      Console.WriteLine("you defeated the Sphinx!");
-      AskRiddle();
+      riddles.RemoveAt(randomNumber);
+      riddleAnswers.RemoveAt(randomNumber);
+      if (riddles.Count == 0)
+      {
+        Console.WriteLine("you defeated the sphinx!");
+      }
+      else
+      {
+        Console.WriteLine("the Sphinx would like to ask you another question!");
+        AskRiddle();
+      }
     }
     else 
     {
